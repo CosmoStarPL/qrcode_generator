69c7b702d647ae638bef85b9d0e10fa4e1b6941d
CosmoStarPL <cosmostarpl@gmail.com>
2024-03-11 03:18:58 +0500

    Added Tkinter

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import utility.qr_code_generator as qcg
+from tkinter import Tk, ttk
 
-qcg.qr_code_generator("www.youtube.com")
+root = Tk()
+
+root.title("QR-code Generator")
+root.iconbitmap("assets/app_icon.ico")
+root.geometry("350x500")
+root.configure(bg='#253547')
+
+label = ttk.Label(root, text="Enter text or link", font=("Manrope", 20), padding=8, foreground="#62A7F4",
+                  background="#253547")
+label.pack()
+
+root.mainloop()
